f8b7d0927d8a4145fde4ec230b332a328eaf57dd
wzyxidian <wzyxidian@gmail.com>
2016-04-16 00:34:53 +0800

    FindKthToTail

--- src/com/edu/xd/sse/lab/offer/printList.java
+++ src/com/edu/xd/sse/lab/offer/printList.java
@@ -9,7 +9,14 @@ import java.util.Stack;
  *输入一个链表，从尾到头打印链表每个节点的值。
  */
 public class printList {
-
+	
+	private class ListNode{
+		int val;
+		ListNode next = null;
+		ListNode(int val){
+			this.val = val;
+		}
+	}
 	/**
 	 * 用栈实现，非递归方式
 	 * @param listNode
@@ -47,10 +54,3 @@ public class printList {
 	}
 }
 
-class ListNode{
-	int val;
-	ListNode next = null;
-	ListNode(int val){
-		this.val = val;
-	}
-}
