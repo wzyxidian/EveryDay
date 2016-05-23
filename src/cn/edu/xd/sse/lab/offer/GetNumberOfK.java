1e618d0251288a900d67b800ce282e960d2dd8ba
wzyxidian <wzyxidian@gmail.com>
2016-05-23 11:09:36 +0800

    new change

--- src/cn/edu/xd/sse/lab/offer/GetNumberOfK.java
+++ src/cn/edu/xd/sse/lab/offer/GetNumberOfK.java
@@ -1,4 +1,4 @@
-package com.edu.xd.sse.lab.offer;
+package cn.edu.xd.sse.lab.offer;
 
 /**
  * @author zhiyong wang
@@ -6,7 +6,12 @@ package com.edu.xd.sse.lab.offer;
  * 题目描述：统计一个数字在排序数组中出现的次数
  */
 public class GetNumberOfK {
-	
+
+    public static void main(String[] args) {
+        GetNumberOfK g = new GetNumberOfK();
+//		g.GetNumberOfK(new int[]{1,2,3,3,3,3,4,5}, 3);
+    }
+    
 	/**
 	 * 要求时间复杂度小于O(n)
 	 * 这里可以采用二分法求得所求k的起始位置下标和
@@ -27,8 +32,8 @@ public class GetNumberOfK {
 	        System.out.println(end - start + 1);
 	        return end - start + 1;
 	    }
-    
-    private int startPositionOfK(int[] array, int low, int high, int k){   
+
+    private int startPositionOfK(int[] array, int low, int high, int k) {
         if(low <= high){
             int mid = (low + high) / 2;
             if(array[mid] < k)
@@ -46,8 +51,8 @@ public class GetNumberOfK {
         }
         return -1;
     }
-	    
-      private int endPositionOfK(int[] array, int low, int high, int k){   
+
+    private int endPositionOfK(int[] array, int low, int high, int k) {
         if(low <= high){
             int mid = (low + high) / 2;
             if(array[mid] < k)
@@ -65,8 +70,4 @@ public class GetNumberOfK {
         }
         return -1;
     }
-      public static void main(String[] args) {
-		GetNumberOfK g = new GetNumberOfK();
-//		g.GetNumberOfK(new int[]{1,2,3,3,3,3,4,5}, 3);
-	}
 }
